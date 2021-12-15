6f162bdf059e9e49150deb19e011bda678ee758f
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-12-15 16:36:11 -0500

    Creación de objetos en Python

--- curso POO uber/Python/main.py
+++ curso POO uber/Python/main.py
@@ -1,2 +1,13 @@
+from car import Car
+
 if __name__ == "__main__":
     print("Hi wordl, I got it")
+    car = Car()
+    car.lincense = "AJK-215"
+    car.driver = "Kebin Martinez"
+    print(vars(car))
+
+    car2 = Car()
+    car2.lincense = "ZSD-555"
+    car2.driver = "Valeria Martinez"
+    print(vars(car2))
