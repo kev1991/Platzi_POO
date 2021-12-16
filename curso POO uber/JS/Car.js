11a68f630622104c72de0622de3c1e84869bf80b
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-12-16 10:53:56 -0500

    Creación de objetos en JavaScript

--- curso POO uber/JS/Car.js
+++ curso POO uber/JS/Car.js
@@ -1,6 +1,11 @@
-function car(){
+function car(lincense, driver){
     this.id;
-    this.lincense;
-    this.drive;
+    this.lincense =lincense;
+    this.drive = driver;
     this.passenger;
+}
+car.prototype.print_data_car = function(){
+    console.log(this.drive);
+    console.log(this.drive.name);
+    console.log(this.drive.document);
 }
---
+++ curso POO uber/JS/Index.js
@@ -0,0 +1,3 @@
+var car = new car("AJK-123", new Account("kebin jose","123456789"));
+car.passenger = 4;
+car.print_data_car();
