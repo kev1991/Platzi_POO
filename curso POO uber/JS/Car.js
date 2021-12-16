437f8cf768f568aa597c3cdde520540c2c33fcd8
Kebin J Martinez T <kebin.martinez.t@gmail.com>
2021-12-16 11:41:36 -0500

    Mejorando las clases en JavaScript

--- curso POO uber/JS/Car.js
+++ curso POO uber/JS/Car.js
@@ -1,11 +1,15 @@
-function car(lincense, driver){
-    this.id;
-    this.lincense =lincense;
-    this.drive = driver;
-    this.passenger;
-}
-car.prototype.print_data_car = function(){
-    console.log(this.drive);
-    console.log(this.drive.name);
-    console.log(this.drive.document);
-}
+class Car{
+
+    constructor (lincense, driver){
+        this.id;
+        this.lincense =lincense;
+        this.drive = driver;
+        this.passenger;
+    }
+
+    print_data_car = function(){
+        console.log(this.drive);
+        console.log(this.drive.name);
+        console.log(this.drive.document);
+    }
+}    
